08601b8337a4681871fe98e9f33dd6b57a310fa1
dance2die <dontbothermewithspam@gmail.com>
2015-05-11 21:17:05 -0400

    Added "Learn.Pluralsight.Bootstrap3.02.Bootstrap3Basics"

---
+++ Learn.Pluralsight.Bootstrap3/Learn.Pluralsight.Bootstrap3.02.Bootstrap3Basics/Global.asax.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace Learn.Pluralsight.Bootstrap3._02.Bootstrap3Basics
+{
+    public class Global : System.Web.HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+        }
+    }
+}
